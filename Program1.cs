c8df21ea0152b63d9d8a055f118baf3415cec77e
rtkazan <126402804+rtkazan@users.noreply.github.com>
2023-03-13 23:35:29 +0300

    Task13

---
+++ Program1.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string nToString = Convert.ToString(n);
+if (nToString.Length > 2)
+{
+    Console.WriteLine(nToString[2]);
+}
+else {
+    Console.WriteLine("Третьей цифры нет");
+}
