254b8873a1c87d1fe7647370343dda3e5c37a417
rtkazan <126402804+rtkazan@users.noreply.github.com>
2023-03-13 23:45:52 +0300

    Add files via upload
    Task 15

---
+++ Program2.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n == 6 || n == 7)
+{
+    Console.WriteLine("да");
+}
+else {
+    Console.WriteLine("нет");
+}
